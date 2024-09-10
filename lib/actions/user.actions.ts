a703f6c0ca2ad552c8e388458cdbc85b80537014
Ahtasham Mustafa <ahtshammustufa786@gmail.com>
2024-09-10 21:33:13 +0500

    sign-in and sign-up Implemented

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,19 @@
+'use server'
+
+export const signIn = async (email: string, password: string) => {
+    
+    try {
+        
+    } catch (error) {
+        console.log('Error in Sign-in==>',error)
+    }
+}
+
+export const signUp = async (userData:SignUpParams) => {
+    
+    try {
+        
+    } catch (error) {
+        console.log('Error in Sign-up==>',error)
+    }
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@
 import { type ClassValue, clsx } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -193,3 +194,19 @@ export const getTransactionStatus = (date: Date) => {
 
   return date > twoDaysAgo ? "Processing" : "Success";
 };
+
+export const authformSchema = (type:string) => z.object({
+  //Sign-Up
+  firstName:type === 'sign-in'? z.string().optional() : z.string().min(3),
+  lastName:type === 'sign-in'? z.string().optional() : z.string().min(3),
+  address:type === 'sign-in'? z.string().optional() : z.string().min(10).max(50),
+  city:type === 'sign-in'? z.string().optional() : z.string().min(3).max(50),
+  state:type === 'sign-in'? z.string().optional() : z.string().min(5).max(13),
+  postalCode:type === 'sign-in'? z.string().optional() : z.string().min(5).max(7),
+  dateOfBirth:type === 'sign-in'? z.string().optional() : z.string().min(10).max(10),
+  cnic:type === 'sign-in'? z.string().optional() : z.string().min(15).max(15),
+  
+  //both
+  email:z.string().email(),
+  password:z.string().min(8, { message: "Password must be at least 8 characters long" }),
+});
