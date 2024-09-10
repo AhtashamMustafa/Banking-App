83d123a31f0386abecdc25284956ae5d6903d946
Ahtasham Mustafa <ahtshammustufa786@gmail.com>
2024-09-10 21:56:22 +0500

    deploy on vercel

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = { firstname: "Ahtasham", lastname: "Mustafa" ,$id:1,email:"ahtasham@example.com",userId:"2",dwollaCustomerUrl:"example",dwollaCustomerId:"example",firstName:"example",lastName:"example",address1:"example",city:"example",state:"example",postalCode:"example",dateOfBirth:"example",ssn:"example"};
+  const loggedIn = { firstname: "Ahtasham", lastname: "Mustafa" ,$id:"1",email:"ahtasham@example.com",userId:"2",dwollaCustomerUrl:"example",dwollaCustomerId:"example",firstName:"example",lastName:"example",address1:"example",city:"example",state:"example",postalCode:"example",dateOfBirth:"example",ssn:"example"};
 
   return (
     <main className="flex h-screen w-full font-inter">
